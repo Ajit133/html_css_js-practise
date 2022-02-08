41af3b0a343c34b0aafe6424f3ba5d053778326d
Ajit133 <dasajit697@gmail.com>
2022-02-09 01:12:54 +0600

    update file

--- script.js
+++ script.js
@@ -6,10 +6,33 @@ if(false){
 
 console.log("Out of Statement!")
 
-var age = prompt("Enter the Age: ")
+// var age = 50;
+// if ( age > 50 ){
+//     console.log("Old")
+// }else if( age < 50 && age >= 30 ){
+//     console.log("Under 50 but above or equal 30!")
+// }else if( age < 40 && age >= 20){
+//     console.log("Undr 40 but above or equal 10!")
+// }else if()
 
-if ( age >= 70 ){
-    console.log("Old")
-}else{
-    console.log("Young!!")
+// Nested if find out largest number 
+var n1 = parseInt(prompt("Enter the Number: "))
+var n2 = parseInt(prompt("Enter the Third Number: "))
+var n3 = parseInt(prompt("Enter the 3rd Number: "))
+ 
+if (n1 >= n2){
+    if(n1 >= n3){
+        console.log(n1 + " is the largest!");
+    }
+    else{
+        console.log(n3 + " is the largest!")
+    }
+
+}else {
+    if ( n2 >= n3){
+        console.log(n2 + " is the largest!")
+    }
+    else{
+        console.log(n3  + " is the largest")
+    }
 }
