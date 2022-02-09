303f9900cf2f5a3fb2087183ea9ead64004b1fb6
Ajit133 <dasajit697@gmail.com>
2022-02-09 11:13:08 +0600

    Update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-if(false){
-    console.log("if statement Executed!")
-}else{
-    console.log("Else statement Executed!")
-}
+// if(false){
+//     console.log("if statement Executed!")
+// }else{
+//     console.log("Else statement Executed!")
+// }
 
-console.log("Out of Statement!")
+// console.log("Out of Statement!")
 
 // var age = 50;
 // if ( age > 50 ){
@@ -16,23 +16,62 @@ console.log("Out of Statement!")
 // }else if()
 
 // Nested if find out largest number 
-var n1 = parseInt(prompt("Enter the Number: "))
-var n2 = parseInt(prompt("Enter the Third Number: "))
-var n3 = parseInt(prompt("Enter the 3rd Number: "))
+// var n1 = parseInt(prompt("Enter the Number: "))
+// var n2 = parseInt(prompt("Enter the Third Number: "))
+// var n3 = parseInt(prompt("Enter the 3rd Number: "))
  
-if (n1 >= n2){
-    if(n1 >= n3){
-        console.log(n1 + " is the largest!");
-    }
-    else{
-        console.log(n3 + " is the largest!")
-    }
+// if (n1 >= n2){
+//     if(n1 >= n3){
+//         console.log(n1 + " is the largest!");
+//     }
+//     else{
+//         console.log(n3 + " is the largest!")
+//     }
 
-}else {
-    if ( n2 >= n3){
-        console.log(n2 + " is the largest!")
-    }
-    else{
-        console.log(n3  + " is the largest")
-    }
+// }else {
+//     if ( n2 >= n3){
+//         console.log(n2 + " is the largest!")
+//     }
+//     else{
+//         console.log(n3  + " is the largest")
+//     }
+// }
+// use of switch case
+// var week = prompt("Enter the Number and get Week day")
+
+// switch(week){
+//     case "0":
+//         day = "Sunday";
+//         break
+//     case "1":
+//         day = "Monday";
+//         break
+//     case "2":
+//         day = "Tuesday";
+//         break
+//     case "3":
+//         day = "Wednesday";
+//         break
+//     case "4":
+//         day = "Thurasday";
+//         break
+//     case "5":
+//         day = "Friday";
+//         break;
+//     case "6":
+//         day = "Saturday";
+//         break
+//     default:
+//         day = "No One Match"
+
+// }
+// console.log(day)
+
+//Use of While loop
+
+// Print "Hello World" 5 many times
+var i = 1
+while(i <= 5){
+    console.log(i);
+    i++
 }
